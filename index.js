3bfd161515445bf7bbcebd26ac68e20f5a99a438
Antti Sykäri <antti.sykari@gmail.com>
2013-05-23 19:02:21 +0300

    Document code, organize, change filter names.
    In anticipation of first implementation of the plugin feature.

--- index.js
+++ index.js
@@ -51,28 +51,63 @@ function escapeSome(text) {
 	return escape(text).replace(escapedCharacterReference, '&$1;');
 }
 
-function filter(text) {
+// The filter that handles most of the "normal" text.  (For example, all of the
+// the text -- apart from the markup -- in headings, list items, and
+// paragraphs.)
+// 
+// The text might contain inline code and HTML tags, which are handled by
+// 'codify' and 'textWithHtml' filters.
+function text(text) {
 	var codified = codify(text);
 
 	var restPattern = /(^|<\/code>)([^]*?)(<code>|$)/g;
 	return codified.replace(restPattern, function(match, p1, p2, p3) {
-		return p1 + normalText(p2) + p3;
+		return p1 + textWithHtml(p2) + p3;
 	});
 }
 
-function normalText(text) {
+// Process text that may contain HTML tags (but not inline code).  Leave the
+// HTML tags alone and run the text sequences in between them through
+// 'plainText' filter.
+function textWithHtml(text) {
 	// Find text between things that look like HTML tags.
 	var betweenPattern = /(^|[^=<>]\s*>)([^]*?)(<\/?\b|$)/g;
 	// This is the right one
+	// TODO what's this?
 	//var betweenPattern = /(^|[^=<>]\s*\/?>)([^]*?)(<\/?\b|$)/g;
 
 	return text.replace(betweenPattern, function(match, p1, p2, p3) {
-		var betweenText = escapeSome(p2);
-		return p1 + dashify(linkify(boldify(italicize(betweenText)))) + p3;
+		return p1 + plainText(p2) + p3;
 	});
 }
 
+var plainTextFilters = [
+	escapeSome,
+	italicize,
+	boldify,
+	linkify,
+	dashify
+]
+
+// Process plain text (which does not contain HTML tags or inline code) by
+// running it through all text filters.
+function plainText(text) {
+	var result = text;
+
+	plainTextFilters.forEach(function(filter) {
+		result = filter(result);
+	});
+
+	return result;
+}
+
+// Replace code blocks (`...`) in text by <code>...</code>.  The contents
+// are run through filter escapeCode.
 function codify(text) {
+	// Match `contents`, where 'contents' does not contain a `.
+	// 'contents' may be contain one ore more escaped `, that is, \`.
+	// Does not match if the first ` is preceded by \.  This lets you quote
+	// `'s both inside and outside of code blocks.
 	var pattern = /([^\\]|^)`([^\\`]*(?:\\.[^`\\]*)*)`/g;
 
 	return text.replace(pattern, function(match, p1, p2) {
@@ -80,16 +115,41 @@ function codify(text) {
 	});
 }
 
+// Replace /contents/ with <i>contents</i>.
+//
+// 'contents' must be non-empty.
+//
+// The starting slash must not be preceded with a word character (\w), and
+// must not be followed by a white space character (\s).  
+// The ending slash must not be preceded with a white space character (\s), and
+// must not be followed by a word character (\w).  
+//
+// Try to not match URLs.  (Though it is possible to contruct a valid URL that
+// will be matched, such as http://foo.com/bar+/zot/.  Eventually we need a way
+// around those.)
+//
+// Slashes that are part of URLs are not matched.
 function italicize(text) {
 	var pattern = /([^<:\/]|^)\B\/(?!\s)([^]*?[^:\/\s])\/\B/g;
 	return text.replace(pattern, '$1<i>$2</i>');
 }
 
+// Replace *contents* with <b>contents</b>.
+//
+// 'contents' must be non-empty.
+//
+// The starting asterisk must not be preceded with a word character (\w), and
+// must not be followed by a white space character (\s).  
+// The ending asterisk must not be preceded with a white space character (\s), and
+// must not be followed by a word character (\w).  
 function boldify(text) {
 	var pattern = /\B\*(?!\s)([^]*?[^\s])\*\B/g;
 	return text.replace(pattern, '<b>$1</b>');
 }
 
+// Replace _link text_ (url) with <a href='url'>link text</a>.
+//
+// URLs 
 function linkify(text) {
 	var pattern = /\b_(?!\s)([^]*?[^\s])_(?:\s*\(((?:[^\(\)]*\((?:[^\(\)]*\([^\(\)]*\))*[^\(\)]*\))*[^\(\)]*)\)|\b)/g;
 
@@ -132,10 +192,9 @@ function handle(element) {
 
 function li(item) {
 	var content = item.replace(/^(\* |[0-9]+. )/, '');
-	return "<li>" + filter(content);
+	return "<li>" + text(content);
 }
 
-// Allowed prefixes: `/^\* /` and `/^#. /`. Those are hardcoded in `separator`.
 function ul(element) {
 	var separator = /\n\* /;
 	var result = "<ul>\n";
@@ -164,13 +223,13 @@ function ol(element) {
 
 var handlers = {
 	paragraph: function(element) {
-		return "<p>" + filter(element);
+		return "<p>" + text(element);
 	},
 	heading: function(element) {
 		var form = /^(#{1,6}) ([^]*)$/;
 		var match = element.match(form);
 		var level = match[1].length;
-		var content = filter(match[2]);
+		var content = text(match[2]);
 		return "<h" + level + ">" + content + "</h" + level + ">";
 	},
 	codeBlock: function(element) {
@@ -222,14 +281,33 @@ function joinCodeBlocks(parts) {
 	return result;
 }
 
+function handleAll(parts) {
+	return parts.map(handle);
+}
+
+function joinParts(parts) {
+	return parts.join('\n') + '\n';
+}
+
+var topLevelFilters = [
+	trim,
+	untabify,
+	split,
+	joinCodeBlocks,
+	handleAll,
+	joinParts
+]
+
+// TODO - replace 'phase' with 'filter' or similar more suitable
+//
 function mk(text) {
-	text = trim(text);
-	text = untabify(text);
-	parts = split(text);
-	parts = joinCodeBlocks(parts);
-	parts = parts.map(handle);
-	text = parts.join('\n') + '\n';
-	return text;
+	result = text;
+
+	topLevelFilters.forEach(function(filter) {
+		result = filter(result);
+	});
+
+	return result;
 }
 
 mk.trim = trim;
@@ -239,8 +317,8 @@ mk.type = type;
 mk.handle = handle;
 mk.escape = escape;
 mk.escapeCode = escapeCode;
-mk.normalText = normalText;
-mk.filter = filter;
+mk.textWithHtml = textWithHtml;
+mk.text = text;
 mk.codify = codify;
 mk.italicize = italicize;
 mk.boldify = boldify;
